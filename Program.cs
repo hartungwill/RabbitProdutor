b6c139b9088e1f21a3ea94a6e82ae8fdfb4a3cc0
William Hartung <hartung.will@gmail.com>
2021-08-05 18:04:38 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitProdutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "qWilliamTest",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    string message = $"Olá Mundo:{i}!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "qWilliamTest",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
